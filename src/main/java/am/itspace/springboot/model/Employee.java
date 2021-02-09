aa41d464874d177f0dd06d7d71b0c79c3d9c2c59
ShahbazyanS <armyanin098@mail.ru>
2021-02-09 09:56:10 +0400

    Create model

---
+++ src/main/java/am/itspace/springboot/model/Employee.java
@@ -0,0 +1,24 @@
+package am.itspace.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+}
