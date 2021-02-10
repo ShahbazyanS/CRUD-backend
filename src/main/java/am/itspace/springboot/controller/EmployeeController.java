8f02fd2eae09ea4fc8012764dacaee26725b8bad
ShahbazyanS <armyanin098@mail.ru>
2021-02-10 12:04:08 +0400

    Update Employee and get all Employee

--- src/main/java/am/itspace/springboot/controller/EmployeeController.java
+++ src/main/java/am/itspace/springboot/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package am.itspace.springboot.controller;
 
+import am.itspace.springboot.exception.ResourceNotFoundException;
 import am.itspace.springboot.model.Employee;
 import am.itspace.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,19 +10,39 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/employees")
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
     @PostMapping("/employees")
-    public Employee createEmloyee(@RequestBody Employee employee){
+    public Employee createEmloyee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable("id") int id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Empolyee with " + id + " id does not exist"));
+    }
+
+    @PutMapping("/employees/{id}")
+    public Employee updateEmployee(@PathVariable int id, @RequestBody Employee employee1) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+
+        employee.setName(employee1.getName());
+        employee.setSurname(employee1.getSurname());
+        employee.setEmail(employee1.getEmail());
+
+        return employeeRepository.save(employee);
+//        return updatedEmployee;
+    }
 }
+
